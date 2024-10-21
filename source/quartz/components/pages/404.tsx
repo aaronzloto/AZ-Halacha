a190889c5cb97d8839e200190985ebd62fc8ca08
aaronzloto <42750636+aaronzloto@users.noreply.github.com>
2024-10-21 18:00:00 -0400

    Update 404.tsx
    updated 404 to go to the desired home page

--- source/quartz/components/pages/404.tsx
+++ source/quartz/components/pages/404.tsx
@@ -5,12 +5,13 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
   const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
   const baseDir = url.pathname
+  const customUrl = `${baseDir}/AZ-Halacha` // Append "/AZ-Halacha" to the base URL
 
   return (
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
+      <a href={customUrl}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
 }
