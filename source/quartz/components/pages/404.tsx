6581bb42b0bd2a38d8652043e4261eae701a7735
aaronzloto <aaronzloto@gmail.com>
2024-10-21 21:31:46 -0400

    Update 404.tsx

--- source/quartz/components/pages/404.tsx
+++ source/quartz/components/pages/404.tsx
@@ -1,18 +1,24 @@
-import { i18n } from "../../i18n";
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types";
+import { i18n } from "../../i18n"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
+<<<<<<< HEAD
   const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`);
   url.pathname += 'AZ-Halacha'; // Directly append to the pathname
+=======
+  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+  const baseDir = url.pathname
+  baseDir += '/AZ-Halacha'
+>>>>>>> parent of bc61fd7 (Update 404.tsx)
 
   return (
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={url.href}>{i18n(cfg.locale).pages.error.home}</a> 
+      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
-  );
-};
+  )
+}
 
-export default (() => NotFound) satisfies QuartzComponentConstructor;
+export default (() => NotFound) satisfies QuartzComponentConstructor
